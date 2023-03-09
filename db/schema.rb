c63307915d44cf1b75beb2b93d44c7764a0af27c
yayahc <hamedcuenca5@gmail.com>
2023-03-09 00:03:35 +0000

    ready to be deploy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_06_204325) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_233529) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -57,6 +57,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_204325) do
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
@@ -67,4 +74,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_06_204325) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "comments", "posts"
+  add_foreign_key "likes", "posts"
 end
